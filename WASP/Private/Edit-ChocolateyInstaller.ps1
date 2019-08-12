174a06e98f2a3339a3ad21f7e216785adb114b64
Kevin Schäfer <kevin.schaefer13@gmail.com>
2019-08-12 18:04:10 +0200

    refactored Edit-ChocolateyInstaller with dynamic additional scripts

--- WASP/Private/Edit-ChocolateyInstaller.ps1
+++ WASP/Private/Edit-ChocolateyInstaller.ps1
@@ -29,13 +29,22 @@ param(
 
     [Parameter()]
     [string]
-    $UnzipPath
+    $UnzipPath,
+
+    [Parameter(Mandatory=$true)]
+    [string[]]
+    $PreAdditionalScripts,
+
+    [Parameter(Mandatory=$true)]
+    [string[]]
+    $PostAddtionalScripts
 )
 
 begin{
-    $NewFile = Join-Path -Path $PackagePath -ChildPath "chocolateyInstall.ps1"
-    $OriginalFile = Join-Path -Path $PackagePath -ChildPath "chocolateyInstall_old.ps1"
-    $ParentSWDirectory = Split-Path -Path (Split-Path -Path $PackagePath)
+    $ChocolateyPackageFolder = Join-Path -Path $PackagePath -ChildPath 'tools'
+    $NewFile = Join-Path -Path $ChocolateyPackageFolder -ChildPath "chocolateyInstall.ps1"
+    $OriginalFile = Join-Path -Path $ChocolateyPackageFolder -ChildPath "chocolateyInstall_old.ps1"
+    $ParentSWDirectory = Split-Path -Path $PackagePath
 } process {
     Copy-Item -Path $NewFile -Destination $OriginalFile
     #Regex
@@ -48,11 +57,13 @@ begin{
     $InstallerContent = $InstallerContent | Where-Object {$_ -notmatch "^\s*#"} | ForEach-Object {$_ -replace '(^.*?)\s*?[^``]#.*','$1'} #| Set-Content -Path $NewFile
     $InstallerContent = $InstallerContent | Where-Object {$_ -notmatch $URLRegex -and $_ -notmatch $ChecksumRegex}  #| Set-Content -Path $NewFile
     $script:FilePathPresent = $false
+    # Check if filepath already present
     $InstallerContent | ForEach-Object {
         if ($_ -match '(file[\s]*=)') {
             $script:FilePathPresent = $true
         }
     }
+    # if filepath is not already present, we have to set the filepath
     if(-Not $script:FilePathPresent) {
         #Write-Log "Calling Set File Path with path $FileName" -Severity 1
         $script:ToolsPathPresent = $false
@@ -82,15 +93,76 @@ begin{
 
     if($UnzipPath) {
         #Write-Log "Calling set unzip location and remove installzip, got unzip location $UnzipPath" -Severity 1
-        $ChocolateyPackageFolder = Join-Path -Path $PackagePath -ChildPath 'tools'
         $InstallerContent -Replace ".*unzipLocation[\s]*=[\s]*Get-PackageCacheLocation","unzipLocation = $ChocolateyPackageFolder"
         $InstallerContent -Replace "Install-ChocolateyZipPackage[\s]*=[\s]@packageArgs",""
     }
 
-    $Versions = Get-ChildItem -Path $ParentSWDirectory -Directory | Select-Object -ExpandProperty $_.Name # | Sort-object $_ -descending | Select-Object -First 2
-    $LastVersions = New-Object System.Collections.ArrayList
-    $Versions | ForEach-Object {$LastVersions.Add([version]$LastVersions)}
-    Write-host $Versions
+    
+
+    # Now we're getting to check if there was already version packaged. If yes we're going to get the last version
+    [string[]]$StringVersions = Get-ChildItem -Path $ParentSWDirectory -Directory | Select-Object -ExpandProperty Name # | Select-Object -ExpandProperty $_.Name # | Sort-object $_ -descending | Select-Object -First 2
+    if($StringVersions.Length -gt 1) {
+        $VersionList = New-Object System.Collections.ArrayList
+        $StringVersions | ForEach-Object {
+            $SplitVersion = $_.Split('.')
+            # Ensure to have minimum x.x or else ps is not able to cast
+            if ($SplitVersion -gt 1) {
+                # Loop through each version part an remove any character which is not a number, so it can be casted.
+                for ($i = 0; $i -lt $SplitVersion.Length; $i++) {
+                    $SplitVersion[$i] = $SplitVersion[$i] -replace "\D+"
+                }
+            }
+            $Version = $SplitVersion -join "."
+            $null = $VersionList.Add([version]$Version)
+        }
+        $VersionList.Sort()
+        $VersionList.Reverse()
+        $LastVersion = $VersionList[1]
+    }
+
+    # Fetch the additional scripts from the last version or create them from scratch
+    $AdditionalScripts = $PreAdditionalScripts + $PostAddtionalScripts
+    if($LastVersion) {
+        $LastVersionPath = Join-Path -Path $ParentSWDirectory -ChildPath "$LastVersion\tools"
+        foreach($AdditionalScript in $AdditionalScripts) {
+            $SourcePath = Join-Path -Path $LastVersionPath -ChildPath $AdditionalScript
+            $DestinationPath = Join-Path -Path $ChocolateyPackageFolder -ChildPath $AdditionalScript
+            if(Test-Path $SourcePath -ErrorAction SilentlyContinue) {
+                Copy-Item -Path $SourcePath -Destination $DestinationPath -Force
+            } else {
+                $null = New-Item -Path $DestinationPath
+            }
+        }
+    } else {
+        foreach($AdditionalScript in $AdditionalScripts) {
+            $ScriptPath = Join-Path -Path $ChocolateyPackageFolder -ChildPath $AdditionalScript            
+            $null = New-Item -Path $ScriptPath -ErrorAction SilentlyContinue
+        }
+    }
+
+    # Fetch the file content raw so we can check with a regex if the additional scripts are already included
+    $InstallerContentRaw = Get-Content -Path $NewFile -Raw
+
+    # Build regex dynamically with all additional scripts
+    $Regex = ""
+    $PreInstallerLine = ""
+    foreach($PreAdditionalScript in $PreAdditionalScripts) {
+        $Regex += ".*$PreAdditionalScript.*\n"
+        $PreInstallerLine += "&(Join-Path `$PSScriptRoot $PreAdditionalScript)`r`n"
+    }
+    $Regex += "Install-Choco"
+    $PostInstallerLine = ""
+    foreach($PostAdditionalScript in $PostAddtionalScripts) {
+        $Regex += ".*\n.*$PostAdditionalScript.*"
+        $PostInstallerLine += "`r`n&(Join-Path `$PSScriptRoot $PostAdditionalScript)"
+    }
+    $PostInstallerLine += "`r`n"
+    $Regex = [regex]$Regex
+    if(-Not $Regex.Matches($InstallerContentRaw).value) {
+        $InstallerLine = $InstallerContent | Where-Object {$_ -match "(I|i)nstall-Choco.*"}
+        $InstallerContent = $InstallerContent -replace $InstallerLine, "$($PreInstallerLine)$($InstallerLine)$($PostInstallerLine)"
+    }
+
     Set-Content -Path $NewFile -Value $InstallerContent
 }
 
