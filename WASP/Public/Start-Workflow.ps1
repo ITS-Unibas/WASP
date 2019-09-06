8fd49843271dffba5fbbe5569d2f299c57e1ff7e
Kevin Schäfer <kevin.schaefer@unibas.ch>
2019-09-06 16:10:53 +0200

    check if new packages null

--- WASP/Public/Start-Workflow.ps1
+++ WASP/Public/Start-Workflow.ps1
@@ -61,8 +61,10 @@ function Start-Workflow {
         # Commit and push changes to wishlist located in the path
         Update-Wishlist $PackageGalleryPath $config.Application.GitBranchPROD
         Write-Log "Found the following new packages: $newPackages"
-        # Initialize branches for each new package
-        Update-PackageInboxFiltered $newPackages
+        if ($newPackages) {
+            # Initialize branches for each new package
+            Update-PackageInboxFiltered $newPackages
+        }
 
         <#
         Start distributing packages to choco servers and promote packages based on package issue position
