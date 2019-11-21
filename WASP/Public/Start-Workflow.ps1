7eff9d304627603b98254656b97117c939f1c734
Kevin Schäfer <kevin.schaefer@unibas.ch>
2019-11-21 13:16:23 +0100

    Changing wishlist workflow

--- WASP/Public/Start-Workflow.ps1
+++ WASP/Public/Start-Workflow.ps1
@@ -106,11 +106,15 @@ function Start-Workflow {
         }
 
         # Commit and push changes to wishlist located in the path
-        Update-Wishlist $PackagesWishlistPath 'master'
         if ($newPackages) {
             Write-Log "Found the following new packages: $($newPackages.ForEach({$_.name}))" -Severity 2
             # Initialize branches for each new package
-            Update-PackageInboxFiltered $newPackages
+            try {
+                Update-PackageInboxFiltered $newPackages
+                Update-Wishlist $PackagesWishlistPath 'master'
+            } catch {
+                Write-Log "Error occurred in Update-PackageInboxFiltered workflow or while updating the wishlist. Please check above for further error messages." -Severity 3
+            }
         }
 
         <#
