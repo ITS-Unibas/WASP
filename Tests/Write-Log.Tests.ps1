7f6b2591f3c24d0a891e4881317a81627474b43f
Maximilian Burgert <max.burgert@unibas.ch>
2020-02-03 16:16:56 +0100

    Beginning with test for writing log

---
+++ Tests/Write-Log.Tests.ps1
@@ -0,0 +1,32 @@
+$path = (Split-Path -Parent $MyInvocation.MyCommand.Path).Replace("\Tests", "\WASP")
+$Private = @(Get-ChildItem -Path $path\Private\*.ps1 -ErrorAction SilentlyContinue)
+
+foreach ($import in $Private) {
+    . $import.fullname
+}
+
+Describe "Writing log to file and console" {
+
+    Mock Write-Host { }
+    Mock Write-EventLog { }
+    Mock Add-Content { }
+
+    It "logs no message if it is empty" {
+        Write-Log "" 0
+        Assert-MockCalled Add-Content -Exactly 0 -Scope It
+        Assert-MockCalled Write-EventLog -Exactly 0 -Scope It
+        Assert-MockCalled Write-Host -Exactly 0 -Scope It
+    }
+    It "logs message to debug as default" {
+        Write-Log "This is nice" 0
+        Assert-MockCalled Add-Content -Exactly 1 -Scope It
+        Assert-MockCalled Write-EventLog -Exactly 0 -Scope It
+        Assert-MockCalled Write-Host -Exactly 1 -Scope It
+    }
+    It "logs message to information when severity is 1 and writes in EventLog" {
+        Write-Log "This is nice" 1
+        Assert-MockCalled Add-Content -Exactly 1 -Scope It
+        Assert-MockCalled Write-EventLog -Exactly 1 -Scope It
+        Assert-MockCalled Write-Host -Exactly 1 -Scope It
+    }
+}
