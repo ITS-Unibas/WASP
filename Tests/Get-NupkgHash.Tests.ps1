cc9a234898582aacdb1ba3370c92af997f3a5595
Maximilian Burgert <max.burgert@unibas.ch>
2020-03-13 13:48:06 +0100

    Adding more tests to hashing of nupkgs

--- Tests/Get-NupkgHash.Tests.ps1
+++ Tests/Get-NupkgHash.Tests.ps1
@@ -7,31 +7,41 @@ foreach ($import in $Private) {
 
 Describe "Getting hash of nupkg" {
 
-  New-Item "TestDrive:\" -Name "sources" -ItemType Directory -Force
-  New-Item "TestDrive:\sources\" -Name "tools" -ItemType Directory -Force
-  New-Item "TestDrive:\sources\" -Name "legal" -ItemType Directory -Force
-  Set-Content "TestDrive:\sources\package.nuspec" -Value "Content nuspec" -Force
-  Set-Content "TestDrive:\sources\tools\chocoInstall.ps1" -Value "Content choco install" -Force
-  Set-Content "TestDrive:\sources\legal\verification.txt" -Value "Content verification" -Force
+  BeforeEach {
+    New-Item "TestDrive:\" -Name "sources" -ItemType Directory -Force
+    New-Item "TestDrive:\sources\" -Name "tools" -ItemType Directory -Force
+    New-Item "TestDrive:\sources\" -Name "legal" -ItemType Directory -Force
+    Set-Content "TestDrive:\sources\package.nuspec" -Value "Content nuspec" -Force
+    Set-Content "TestDrive:\sources\tools\chocoInstall.ps1" -Value "Content choco install" -Force
+    Set-Content "TestDrive:\sources\legal\verification.txt" -Value "Content verification" -Force
+    Set-Content "TestDrive:\sources\.README" -Value "READ ME PLEASE" -Force
 
-  New-Item "TestDrive:\" -Name "nupkg" -ItemType Directory
+    New-Item "TestDrive:\" -Name "nupkg" -ItemType Directory
 
-  Compress-Archive -Path "TestDrive:\sources\*" -DestinationPath "TestDrive:\nupkg\package.zip" -Force
-  Rename-Item -Path "TestDrive:\nupkg\package.zip" -NewName "package.nupkg"
+    Compress-Archive -Path "TestDrive:\sources\*" -DestinationPath "TestDrive:\nupkg\package.zip" -Force
+    Rename-Item -Path "TestDrive:\nupkg\package.zip" -NewName "package.nupkg"
 
-  $nupkgPath = "TestDrive:\nupkg\package.nupkg"
-  $packageFolder = "TestDrive:\nupkg"
+    $nupkgPath = "TestDrive:\nupkg\package.nupkg"
+    $packageFolder = "TestDrive:\nupkg"
+  }
+
+  AfterEach {
+    Remove-Item "TestDrive:\*" -Recurse -Force
+  }
 
   It "hashes contents of nupkg" {
     $hash = Get-NupkgHash $nupkgPath $packageFolder
     $hash | Should -Be -Not $null
   }
 
-  <#
   It "changes to the readme do not have an effect to the hash" {
     $hash = Get-NupkgHash $nupkgPath $packageFolder
+    Set-Content "TestDrive:\sources\.README" -Value "READ ME PLEASE!" -Force
+    $newhash = Get-NupkgHash $nupkgPath $packageFolder
+    $hash | Should -BeExactly $newhash
   }
 
+  <#
   It "tests hash for changes to file in \tools directory" {
     $hash = Get-NupkgHash $nupkgPath $packageFolder
   }
