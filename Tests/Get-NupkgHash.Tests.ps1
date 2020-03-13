0869d1b0f61d96708bd23e0d50fa3f3323c92dc4
Maximilian Burgert <max.burgert@unibas.ch>
2020-03-13 14:04:37 +0100

    Finishing nupkg hash tests

--- Tests/Get-NupkgHash.Tests.ps1
+++ Tests/Get-NupkgHash.Tests.ps1
@@ -34,20 +34,53 @@ Describe "Getting hash of nupkg" {
     $hash | Should -Be -Not $null
   }
 
-  It "changes to the readme do not have an effect to the hash" {
+  It "has the same hash if nothing is changed" {
     $hash = Get-NupkgHash $nupkgPath $packageFolder
-    Set-Content "TestDrive:\sources\.README" -Value "READ ME PLEASE!" -Force
+    Remove-Item "TestDrive:\*" -Recurse -Force
+
+    New-Item "TestDrive:\" -Name "sources" -ItemType Directory -Force
+    New-Item "TestDrive:\sources\" -Name "tools" -ItemType Directory -Force
+    New-Item "TestDrive:\sources\" -Name "legal" -ItemType Directory -Force
+    Set-Content "TestDrive:\sources\package.nuspec" -Value "Content nuspec" -Force
+    Set-Content "TestDrive:\sources\tools\chocoInstall.ps1" -Value "Content choco install" -Force
+    Set-Content "TestDrive:\sources\legal\verification.txt" -Value "Content verification" -Force
+    Set-Content "TestDrive:\sources\.README" -Value "READ ME PLEASE" -Force
+
+    New-Item "TestDrive:\" -Name "nupkg" -ItemType Directory
+
+    Compress-Archive -Path "TestDrive:\sources\*" -DestinationPath "TestDrive:\nupkg\package.zip" -Force
+    Rename-Item -Path "TestDrive:\nupkg\package.zip" -NewName "package.nupkg"
     $newhash = Get-NupkgHash $nupkgPath $packageFolder
     $hash | Should -BeExactly $newhash
   }
 
-  <#
+  It "changes to the readme do not have an effect to the hash" {
+    $hash = Get-NupkgHash $nupkgPath $packageFolder
+    Remove-Item "TestDrive:\nupkg\package.nupkg" -Recurse -Force
+    Set-Content "TestDrive:\sources\.README" -Value "READ ME PLEASE!" -Force
+    Compress-Archive -Path "TestDrive:\sources\*" -DestinationPath "TestDrive:\nupkg\package.zip" -Force
+    Rename-Item -Path "TestDrive:\nupkg\package.zip" -NewName "package.nupkg" -Force
+    $newhash = Get-NupkgHash $nupkgPath $packageFolder
+    $hash | Should -BeExactly $newhash
+  }
+
   It "tests hash for changes to file in \tools directory" {
     $hash = Get-NupkgHash $nupkgPath $packageFolder
+    Remove-Item "TestDrive:\nupkg\package.nupkg" -Recurse -Force
+    Set-Content "TestDrive:\sources\tools\chocoInstall.ps1" -Value "Content choco install altered" -Force
+    Compress-Archive -Path "TestDrive:\sources\*" -DestinationPath "TestDrive:\nupkg\package.zip" -Force
+    Rename-Item -Path "TestDrive:\nupkg\package.zip" -NewName "package.nupkg" -Force
+    $newhash = Get-NupkgHash $nupkgPath $packageFolder
+    $hash | Should -Not -BeExactly $newhash
   }
 
   It "tests hash for changes to file in \legal directory" {
     $hash = Get-NupkgHash $nupkgPath $packageFolder
+    Remove-Item "TestDrive:\nupkg\package.nupkg" -Recurse -Force
+    Set-Content "TestDrive:\sources\legal\verification.txt" -Value "Content verification altered" -Force
+    Compress-Archive -Path "TestDrive:\sources\*" -DestinationPath "TestDrive:\nupkg\package.zip" -Force
+    Rename-Item -Path "TestDrive:\nupkg\package.zip" -NewName "package.nupkg" -Force
+    $newhash = Get-NupkgHash $nupkgPath $packageFolder
+    $hash | Should -Not -BeExactly $newhash
   }
-  #>
 }
