fc32356e9b251457c88bd25b0c94db32ef1badb3
Kevin Schäfer <kevin.schaefer@unibas.ch>
2020-05-20 13:53:13 +0200

    Change Install Argument

--- WASP/Private/Edit-ChocolateyInstaller.ps1
+++ WASP/Private/Edit-ChocolateyInstaller.ps1
@@ -91,7 +91,7 @@ function Edit-ChocolateyInstaller {
             if ($UnzipPath) {
                 Write-Log "Calling set unzip location and remove installzip, got unzip location $UnzipPath" -Severity 1
                 $InstallerContent = $InstallerContent | ForEach-Object { $_ -replace '.*unzipLocation[\s]*=[\s]*Get-PackageCacheLocation', "unzipLocation = $UnzipPath" }
-                $InstallerContent = $InstallerContent | ForEach-Object { $_ -replace 'Install-ChocolateyZipPackage\s*@packageArgs', 'Install-ChocolateyPackage @packageArgs' }
+                $InstallerContent = $InstallerContent | ForEach-Object { $_ -replace 'Install-ChocolateyZipPackage\s*@packageArgs', 'Install-ChocolateyInstallPackage @packageArgs' }
             }
 
             # Now we're getting to check if there was already version packaged. If yes we're going to get the last version
