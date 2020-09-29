0e28cca94cae1321c6e4c3a6a7a6a96a974633ac
Maximilian Burgert <max.burgert@unibas.ch>
2020-09-29 08:28:38 +0200

    Adding ideas

--- WASP/Private/Copy-PreviousInstallerFile.ps1
+++ WASP/Private/Copy-PreviousInstallerFile.ps1
@@ -16,7 +16,10 @@ function Copy-PreviousInstallerFile {
 
         # check if msi or exe package:
         $InstallerContent = Get-Content -Path $InstallerFilePath -ErrorAction Stop
+
         # If content contains exe or msi, adapt the filter
+        #$InstallerLine = $InstallerContent | Where-Object { $_ -match "(I|i)nstall-Choco.*" }
+        #$InstallerContent = $InstallerContent -replace $InstallerLine, "$($PreInstallerLine)$($InstallerLine)`r`n`$packageArgs.file = `$fileLocation`r`nInstall-ChocolateyInstallPackage @packageArgs`r`n$($PostInstallerLine)"
 
         # Replace file with $file = Get-ChildItem . -Filter "*.msi" | Select-Object -ExpandProperty FullName
         $file = Get-ChildItem $toolsPath -Filter "*.msi" | Select-Object -ExpandProperty FullName
