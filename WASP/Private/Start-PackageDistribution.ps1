e42270b7b414b01649ad8fc665dd206d20e9d167
uwe_molnar <76432776+uwemolnar@users.noreply.github.com>
2021-10-19 07:37:56 +0200

    fixed version-comparison and changed from 5 to last 3 versions (#12)

--- WASP/Private/Start-PackageDistribution.ps1
+++ WASP/Private/Start-PackageDistribution.ps1
@@ -176,7 +176,7 @@ function Start-PackageDistribution() {
                     $packagePath = Join-Path $PackageGalleryPath $package
                     $versionsList = Get-ChildItem $packagePath -Directory
                     #TODO: Add changes to version history here
-                    $versionsList | Sort-Object -Property { $_.Name -as [version] } | Select-Object -Last 5
+                    $versionsList = ($versionsList | Sort-Object -Property { $_.Name -as [version] } | Select-Object -Last 3)
                     foreach ($version in $versionsList) {
                         if (Test-ExistPackageVersion $GitFolderName $package $version $branch) {
                             $packageRootPath = Join-Path $packagePath $version
