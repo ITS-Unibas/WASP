276b8d5bdf03fe7e3b37effa23df1c4df9e66833
Maximilian Burgert <max.burgert@unibas.ch>
2021-11-23 13:55:20 +0100

    Adds ability to set log level on watcher-side

--- WASP/Public/Start-ChocoLogWatcher.ps1
+++ WASP/Public/Start-ChocoLogWatcher.ps1
@@ -16,17 +16,23 @@ function Start-ChocoLogWatcher {
     param(
         [Parameter()]
         [int]
-        $LineCount = 1
+        $LineCount = 1,
+
+        # Sets the log level. Default 0 (Debug). Possible values: 0 (Debug), 1 (Information), 2 (Warning), 3 (Error)
+        [Parameter()]
+        [ValidateSet(0, 1, 2, 3)]
+        [int]
+        $LogLevel = 0
     )
 
     $Config = Read-ConfigFile
     $LogPath = Join-Path -Path $Config.Application.BaseDirectory -ChildPath $Config.Logger.LogSubFilePath
     $LogFile = Get-ChildItem $LogPath | Select-Object -Last 1 | Select-Object -ExpandProperty FullName
     Get-Content $LogFile -Tail $LineCount -Wait | Where-Object {
-        if ($_ -match "Debug") { Write-Host $_ -ForegroundColor Cyan }
-        elseif ($_ -match "Information") { Write-Host $_ -ForegroundColor Magenta }
-        elseif ($_ -match "Warning") { Write-Host $_ -ForegroundColor Yellow }
-        elseif ($_ -match "Error") { Write-Host $_ -ForegroundColor Red }
+        if ($_ -match "Debug" -and $LogLevel -eq 0) { Write-Host $_ -ForegroundColor Cyan }
+        elseif ($_ -match "Information" -and $LogLevel -le 1) { Write-Host $_ -ForegroundColor Magenta }
+        elseif ($_ -match "Warning" -and $LogLevel -le 2) { Write-Host $_ -ForegroundColor Yellow }
+        elseif ($_ -match "Error" -and $LogLevel -le 3) { Write-Host $_ -ForegroundColor Red }
         else { Write-Host $_ -ForegroundColor White }
     }
 }
