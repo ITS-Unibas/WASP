6caa5663daadff1321a0a4edb8ab8197720e510c
Uwe Molnar - Unibas <108866933+umol-unibas@users.noreply.github.com>
2024-12-20 14:19:46 +0100

    Update Install-ChocolateyZipPackage.ps1

--- WASP/Private/Install-ChocolateyZipPackage.ps1
+++ WASP/Private/Install-ChocolateyZipPackage.ps1
@@ -117,10 +117,10 @@ function Install-ChocolateyZipPackage() {
     }
 
     # Check the url found above ($url or $url64bit) and download the file
-    if ($null -eq $url) {
-        $urlFound = $url64bit
-    } elseif ($null -eq $url64bit) {
+    if ($null -ne $url) {
         $urlFound = $url
+    } elseif ($null -ne $url64bit) {
+        $urlFound = $url64bit
     }    
 
     Write-Log "Start editing chocolateyInstall..." -Severity 1
