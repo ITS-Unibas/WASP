9f7f01336116c98dafd8beee5e9ba3f5a2e572ba
julianbopp <julian.bopp@tutanota.com>
2025-09-09 16:47:55 +0200

    No new branch for package not in wishlist

--- WASP/Private/New-RemoteBranch.ps1
+++ WASP/Private/New-RemoteBranch.ps1
@@ -30,9 +30,36 @@ function New-RemoteBranch {
     begin {
         $Config = Read-ConfigFile
         $RemoteBranches = Get-RemoteBranches -Repo $Repository -User $User
+
+        $GitRepo = $config.Application.PackagesWishlist
+        $GitFile = $GitRepo.Substring($GitRepo.LastIndexOf("/") + 1, $GitRepo.Length - $GitRepo.LastIndexOf("/") - 1)
+        $WishlistFolderName = $GitFile.Replace(".git", "")
+        $PackagesWishlistPath = Join-Path -Path $config.Application.BaseDirectory -ChildPath $WishlistFolderName
+        $wishlistPath = Join-Path -Path  $PackagesWishlistPath -ChildPath "wishlist.txt"
     }
 
     process {
+
+        $wishlist = Get-Content -Path $wishlistPath | Where-Object { $_ -notlike "#*" }
+        $nameAndVersionSeparator = '@'
+        
+        $packageName = $packageName -replace "$nameAndVersionSeparator.*", ""
+        $packageName = $packageName -replace "dev/", ""
+        $foundInWishlist = $false
+        foreach ($line in $wishlist) {
+            $line = $line -replace "@.*", ""
+            if ($line -eq $packageName) {
+                $foundInWishlist = $true
+            }
+        }
+
+        if (-not $foundInWishlist) {
+            $IssueKey = Get-JiraIssueKeyFromName -issueName $packageName 
+            Write-Log "Package $packageName not found in wishlist. Will not create branch." -Severity 2
+            Flag-JiraTicket -issueKey $IssueKey -comment "Package $packageName not found in wishlist. Will not create branch." 
+            return
+        }
+
         if ($RemoteBranches.Contains($BranchName)) {
             Write-Log "Branch $branchName already exist. Nothing to do"
             return
