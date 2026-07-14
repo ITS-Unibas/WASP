fea857e72d104cee24c994e2e13ce1dbf44e9c50
Uwe Molnar - Unibas <108866933+umol-unibas@users.noreply.github.com>
2026-07-14 11:28:40 +0200

    Update Search-Wishlist.ps1

--- WASP/Private/Search-Wishlist.ps1
+++ WASP/Private/Search-Wishlist.ps1
@@ -87,7 +87,7 @@ function Search-Wishlist {
 						$tempPackageFromated = ""
 							
 						$tempPackage = New-Object psobject @{'path' = 'none'; 'name' = $packageName; 'version' = $packageVersion}
-						$tempPackageFromated = Format-Version -packages $tempPackage
+						$tempPackageFromated = Format-Version -packages $tempPackage -noFilesAndFolderUpdate $true
 						
                         if (([version($tempPackageFromated.version)) -le ([version]$previousVersion)) {
                             continue
